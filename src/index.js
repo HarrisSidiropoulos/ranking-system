7b8f69cec611dcf09b2601def181c88d3b7da649
harris sidiropoulos <harris.sidiropoulos@gmail.com>
2017-07-08 11:32:07 +0300

    first commit

---
+++ src/index.js
@@ -0,0 +1,16 @@
+// TODO: create the User class/object
+// it must support rank, progress and the incProgress(rank) method
+
+class User {
+  rank () {
+
+  }
+  progress () {
+
+  }
+  incProgress (rank) {
+    return rank;
+  }
+}
+
+export default User;
---
+++ src/index.test.js
@@ -0,0 +1,31 @@
+import { expect, } from 'chai';
+import User from './index';
+
+describe('User Class', () => {
+  const user = new User();
+
+  describe('class', () => {
+    it('should not be undefined', () => {
+      expect(user).to.not.be.an('undefined');
+    });
+    it('should be an object', () => {
+      expect(user).to.be.an('object');
+    });
+    describe('public method rank', () => {
+      it('should not be undefined', () => {
+        expect(user.rank).to.not.be.an('undefined');
+      });
+      it('should be a function', () => {
+        expect(user.rank).to.be.a('function');
+      });
+    });
+    describe('public method progress', () => {
+      it('should not be undefined', () => {
+        expect(user.progress).to.not.be.an('undefined');
+      });
+      it('should be a function', () => {
+        expect(user.progress).to.be.a('function');
+      });
+    });
+  });
+});
