9fd5b5b3174df82f9114d1e052463fe33ad5cfc4
harris sidiropoulos <harris.sidiropoulos@gmail.com>
2017-07-08 20:14:01 +0300

    sollution

--- src/index.js
+++ src/index.js
@@ -2,15 +2,33 @@
 // TODO: create the User class/object
 // it must support rank, progress and the incProgress(rank) method
 
+const MAX = 8;
+const MIN = -8;
+
 class User {
-  rank() {
-
-  }
-  progress() {
-
+  constructor(rank = MIN, progress = 0) {
+    this.rank = rank;
+    this.progress = progress;
   }
   incProgress(rank) {
-    return rank;
+    if (rank > MAX || rank < MIN || rank === 0) {
+      throw new Error('Invalid rank');
+    }
+    const r = this.rank < 0 && rank > 0 ? rank - 1 : rank;
+    const d = r - this.rank;
+    if (d === -1 || (rank === -1 && this.rank === 1)) {
+      this.progress += 1;
+    } else if (d === 0) {
+      this.progress += 3;
+    } else if (d > 0) {
+      this.progress += 10 * d * d;
+    }
+    while (this.progress >= 100) {
+      this.progress -= 100;
+      this.rank += this.rank === -1 ? 2 : 1;
+    }
+    this.rank = Math.min(MAX, this.rank);
+    this.progress = this.rank === MAX ? 0 : this.progress;
   }
 }
 
--- src/index.test.js
+++ src/index.test.js
@@ -3,7 +3,10 @@ import User from './index';
 
 describe('User Class', () => {
   const user = new User();
-
+  beforeEach(() => {
+    user.rank = -8;
+    user.progress = 0;
+  });
   describe('class', () => {
     it('should not be undefined', () => {
       expect(user).to.not.be.an('undefined');
@@ -15,16 +18,153 @@ describe('User Class', () => {
       it('should not be undefined', () => {
         expect(user.rank).to.not.be.an('undefined');
       });
-      it('should be a function', () => {
-        expect(user.rank).to.be.a('function');
-      });
     });
     describe('public method progress', () => {
       it('should not be undefined', () => {
         expect(user.progress).to.not.be.an('undefined');
       });
-      it('should be a function', () => {
-        expect(user.progress).to.be.a('function');
+    });
+    describe('Class properties rank and progress initial values', () => {
+      describe('rank', () => {
+        it('should be -8', () => {
+          expect(user.rank).to.be.equal(-8);
+        });
+      });
+      describe('progress', () => {
+        it('should be 0', () => {
+          expect(user.progress).to.be.equal(0);
+        });
+      });
+    });
+  });
+  describe('Completing an activity that is ranked the same as that of the user\'s will be worth 3 points', () => {
+    describe('progress', () => {
+      it('should be 3', () => {
+        user.incProgress(-8);
+        expect(user.progress).to.be.equal(3);
+      });
+    });
+  });
+  describe('Completing an activity that is ranked one ranking lower than the user\'s will be worth 1 point', () => {
+    describe('progress', () => {
+      it('should be 1', () => {
+        user.rank = -7;
+        user.incProgress(-8);
+        expect(user.progress).to.be.equal(1);
+      });
+    });
+  });
+  describe('Any activities completed that are ranking 2 levels or more lower than the user\'s ranking will be ignored', () => {
+    describe('progress', () => {
+      it('should be 0', () => {
+        user.rank = -6;
+        user.incProgress(-8);
+        expect(user.progress).to.be.equal(0);
+      });
+    });
+  });
+  describe('Completing an activity ranked higher than the current user\'s rank will accelerate the rank progression. ', () => {
+    describe('If a user ranked -8 completes an activity ranked -7 they will receive 10 progress', () => {
+      describe('progress', () => {
+        it('should be 10', () => {
+          user.incProgress(-7);
+          expect(user.progress).to.be.equal(10);
+        });
+      });
+    });
+    describe('If a user ranked -8 completes an activity ranked -6 they will receive 40 progress', () => {
+      describe('progress', () => {
+        it('should be 40', () => {
+          user.incProgress(-6);
+          expect(user.progress).to.be.equal(40);
+        });
+      });
+    });
+    describe('If a user ranked -8 completes an activity ranked -5 they will receive 90 progress', () => {
+      describe('progress', () => {
+        it('should be 90', () => {
+          user.incProgress(-5);
+          expect(user.progress).to.be.equal(90);
+        });
+      });
+    });
+    describe(`If a user ranked -8 completes an activity ranked -4 they
+              will receive 160 progress, resulting in the user being upgraded
+              to rank -7 and having earned 60 progress towards their next rank`, () => {
+      describe('progress', () => {
+        it('should be 100 when rank is -8', () => {
+          user.incProgress(-4);
+          expect(user.progress).to.be.equal(60);
+        });
+      });
+      describe('rank', () => {
+        it('should be 100 when rank is -8', () => {
+          user.incProgress(-4);
+          expect(user.rank).to.be.equal(-7);
+        });
+      });
+    });
+    describe('If a user ranked -1 completes an activity ranked 1 they will receive 10 progress (remember, zero rank is ignored)', () => {
+      describe('progress', () => {
+        it('should be 10', () => {
+          user.rank = -1;
+          user.incProgress(1);
+          expect(user.progress).to.be.equal(10);
+        });
+      });
+    });
+    describe('Icrease rank 10 times', () => {
+      describe('rank', () => {
+        it('should be -2', () => {
+          user.incProgress(1);
+          expect(user.rank).to.be.equal(-2);
+        });
+      });
+    });
+    describe('Icrease rank 10 times', () => {
+      beforeEach(() => {
+        for (let i = 0; i < 10; i += 1) {
+          user.incProgress(-7);
+        }
+      });
+      describe('progress', () => {
+        it('should be 0', () => {
+          expect(user.progress).to.be.equal(0);
+        });
+      });
+      describe('rank', () => {
+        it('should be -7', () => {
+          expect(user.rank).to.be.equal(-7);
+        });
+      });
+    });
+    describe('Icrease rank 11 times', () => {
+      beforeEach(() => {
+        for (let i = 0; i < 11; i += 1) {
+          user.incProgress(-7);
+        }
+      });
+      describe('progress', () => {
+        it('should be 3', () => {
+          expect(user.progress).to.be.equal(3);
+        });
+      });
+      describe('rank', () => {
+        it('should be -7', () => {
+          expect(user.rank).to.be.equal(-7);
+        });
+      });
+    });
+  });
+  describe('Test rank before zero', () => {
+    beforeEach(() => {
+      user.rank = 1;
+      user.progress = 20;
+      user.incProgress(-1);
+    });
+    describe('progress', () => {
+      it('should be 21', () => {
+        expect(user.progress).to.be.equal(21);
       });
     });
   });
